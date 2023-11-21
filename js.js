08763e6f3bcf5629c7f8ea96737fac2f198015bf
Linda-Rabia <tetalindar.10@gmail.com>
2023-11-21 16:25:53 +0200

    My Doings

---
+++ js.js
@@ -0,0 +1,17 @@
+console.log("Linda")
+var testVar;
+testVar= 2;
+console.log(testVar)
+var hello = null;
+console.log("hello")
+
+
+
+let See;
+console.log(See)
+let herBe;
+herBe = 4;
+console.log(herBe)
+let linda = 25;
+linda = 20;
+console.log(linda);
