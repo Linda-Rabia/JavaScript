ac10583cd38be3d01547ebd498e9ba94956c68b3
Linda-Rabia <tetalindar.10@gmail.com>
2023-11-27 12:10:47 +0200

    work updated

--- js.js
+++ js.js
@@ -1,17 +1,53 @@
-console.log("Linda")
-var testVar;
-testVar= 2;
-console.log(testVar)
-var hello = null;
-console.log("hello")
-
-
-
-let See;
-console.log(See)
-let herBe;
-herBe = 4;
-console.log(herBe)
-let linda = 25;
-linda = 20;
-console.log(linda);
+//If we want to store text, we must enclose it in single or double quotes or in backticks.
+console.log("Linda");
+console.log("hello");
+// checking the type of a variable that stores text.
+let text = "hello";
+console.log(typeof text);
+// reassign variables with Booleans.
+let isOnSale = true;
+console.log(isOnSale);
+isOnSale = false;
+console.log(isOnSale);
+let isTrue;
+isTrue = true;
+console.log(isTrue);
+console.log(Boolean(100));
+console.log(Boolean(100));
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean(-5));
+//The first operator we explore is the Equal to (==) operator. It checks if two values are equal.
+const priceOne = 5;
+const priceTwo = 10;
+console.log(priceOne == priceTwo);
+//Now, priceOne and priceTwo have the same value.
+const priceThree = 10;
+const priceFour = 10;
+console.log(priceThree == priceFour);
+// the Not Equal to (!=) operator. It checks if two values are not equal. 
+const priceFive = 5;
+const priceSix = 10;
+console.log(priceFive != priceSix);
+// The Greater than (>) operator checks if the left value is greater than the right value.
+const priceSeven = 5;
+const priceEight = 10;
+ console.log(priceSeven > priceEight);
+console.log(priceEight > priceSeven);
+//The Less than (<) operator checks if the left value is less than the right value.
+const priceNine = 5;
+const priceTen = 10;
+ 
+console.log(priceNine < priceTen);
+console.log(priceTen < priceNine);
+// there are also the Less than or equal (<=) and Greater than or equal (>=) operators. 
+const priceEleven = 10;
+const priceTwelve = 15;
+const priceThirteen = 10;
+ 
+console.log(priceEleven >= priceTwelve);
+console.log(priceEleven >= priceThirteen);
+//the typeof operator is used to find out the type of a value or variable.
+console.log(typeof 1);
+const num = 1;
+console.log(typeof num);
